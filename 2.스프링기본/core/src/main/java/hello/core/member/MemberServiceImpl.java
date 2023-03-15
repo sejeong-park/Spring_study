0049cb3621a4b6fe44994ab3710009a149170b25
sejeong-park <tpwjd29@gmail.com>
2023-03-15 23:29:25 +0900

    [기본편] 스프링 핵심 원리와 이해 1 - 예제 만들기

---
+++ 2.스프링기본/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    // MemberServiceImple에서 회원 가입하고, 조회하려면
+    // MemberRepository에서 만들었던 구현 객체를 선언해야 한다.
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
---
+++ 2.스프링기본/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    // store는 해쉬
+    private static Map<Long, Member> store = new HashMap<>(); //  실무에서는 동시성 이슈가 있을 수 있기 때문에 concurrent HashMap
+
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member); // id와 member 넣기
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
